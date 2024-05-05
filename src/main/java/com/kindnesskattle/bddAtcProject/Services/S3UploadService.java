730074bf02f59b88dcfef375eea0c63cdf2e3015
imkrsnna <krishna.singh@bbd.co.za>
2024-05-05 11:33:45 +0530

    created service for uploading images into s3 buket

--- src/main/java/com/kindnesskattle/bddAtcProject/Services/S3UploadService.java
+++ src/main/java/com/kindnesskattle/bddAtcProject/Services/S3UploadService.java
@@ -2,13 +2,12 @@ package com.kindnesskattle.bddAtcProject.Services;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 @Service
 public class S3UploadService {
@@ -19,11 +18,52 @@ public class S3UploadService {
         this.s3Client = AmazonS3ClientBuilder.defaultClient();
     }
 
-    public void uploadPhoto(String bucketName, String keyName, MultipartFile file) {
+    public void uploadPhotoToProfiles(String fileName, MultipartFile file) {
+        String keyName = "Profiles/" + fileName;
+        uploadPhoto("unique-kindnesskettle-image", keyName, file);
+    }
+
+    public void uploadPhotoToFoodPost(String fileName, MultipartFile file) {
+        String keyName = "FoodPost/" + fileName;
+        uploadPhoto("unique-kindnesskettle-image", keyName, file);
+    }
+
+    public File downloadFileFromProfiles(String fileName) {
+        String keyName = "Profiles/" + fileName;
+        return downloadFile("unique-kindnesskettle-image", keyName);
+    }
+
+    public File downloadFileFromFoodPost(String fileName) {
+        String keyName = "FoodPost/" + fileName;
+        return downloadFile("unique-kindnesskettle-image", keyName);
+    }
+
+    private void uploadPhoto(String bucketName, String keyName, MultipartFile file) {
         try {
             s3Client.putObject(new PutObjectRequest(bucketName, keyName, file.getInputStream(), new ObjectMetadata()));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private File downloadFile(String bucketName, String keyName) {
+        File tempFile = null;
+        try {
+            S3Object object = s3Client.getObject(new GetObjectRequest(bucketName, keyName));
+            InputStream inputStream = object.getObjectContent();
+            tempFile = File.createTempFile("temp", null);
+            OutputStream outputStream = new FileOutputStream(tempFile);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException | AmazonS3Exception e) {
+            e.printStackTrace();
+        }
+        return tempFile;
+    }
+
 }
