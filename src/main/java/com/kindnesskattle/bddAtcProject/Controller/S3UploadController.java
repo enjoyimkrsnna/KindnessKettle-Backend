3ad1f0dfc91a50c68bfc540d22d96b8bb1bf4fa7
imkrsnna <krishna.singh@bbd.co.za>
2024-05-05 12:01:54 +0530

    adding download method for images from s3

--- src/main/java/com/kindnesskattle/bddAtcProject/Controller/S3UploadController.java
+++ src/main/java/com/kindnesskattle/bddAtcProject/Controller/S3UploadController.java
@@ -3,8 +3,16 @@ package com.kindnesskattle.bddAtcProject.Controller;
 import com.kindnesskattle.bddAtcProject.Services.S3UploadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping("/kindnessKettle")
@@ -38,6 +46,49 @@ public class S3UploadController {
         return "Photo uploaded successfully to FoodPost folder.";
     }
 
+    @GetMapping("/downloadFromProfiles")
+    public ResponseEntity<InputStreamResource> downloadFromProfiles(@RequestParam String fileName) {
+        log.info("Received request to download file '{}' from Profiles folder.", fileName);
 
+        File file = s3UploadService.downloadFileFromProfiles(fileName);
+        if (file != null) {
+            try {
+                InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                        .contentLength(file.length())
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(resource);
+            } catch (FileNotFoundException e) {
+                log.error("File '{}' not found in Profiles folder.", fileName);
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            log.error("Failed to download file '{}' from Profiles folder.", fileName);
+            return ResponseEntity.notFound().build();
+        }
+    }
 
+    @GetMapping("/downloadFromFoodPost")
+    public ResponseEntity<InputStreamResource> downloadFromFoodPost(@RequestParam String fileName) {
+        log.info("Received request to download file '{}' from FoodPost folder.", fileName);
+
+        File file = s3UploadService.downloadFileFromFoodPost(fileName);
+        if (file != null) {
+            try {
+                InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                        .contentLength(file.length())
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(resource);
+            } catch (FileNotFoundException e) {
+                log.error("File '{}' not found in FoodPost folder.", fileName);
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            log.error("Failed to download file '{}' from FoodPost folder.", fileName);
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
