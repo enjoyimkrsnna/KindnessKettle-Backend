5c3f96123c8b154042ac068be6a81fda85fba647
apsinghrana100@gmail.com <ajay.singh@bbd.co.za>
2024-05-11 13:33:49 +0530

    webconfig cors

---
+++ src/main/java/com/kindnesskattle/bddAtcProject/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.kindnesskattle.bddAtcProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow only specific methods
+    }
+}
