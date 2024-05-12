da0ee23bce860cb1a8ae6cb6fbbbbd5b7a5ae05f
imkrsnna <krishna.singh@bbd.co.za>
2024-05-12 15:25:23 +0530

    adding auth to ec2

--- src/main/java/com/kindnesskattle/bddAtcProject/config/WebConfig.java
+++ src/main/java/com/kindnesskattle/bddAtcProject/config/WebConfig.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Allow requests from any origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow only specific methods
+                .allowedOrigins("*")
+                .allowedOrigins("http://127.0.0.1:5500") // Allow requests from your frontend domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow only specific methods
+                .allowCredentials(true); // Allow credentials (e.g., cookies)
     }
 }
