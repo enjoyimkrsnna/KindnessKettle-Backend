06518f40b86afe414a4bf0ad3065fa7c75bee1eb
imkrsnna <krishna.singh@bbd.co.za>
2024-05-18 21:06:03 +0530

    allowing all origin

--- src/main/java/com/kindnesskattle/bddAtcProject/config/WebConfig.java
+++ src/main/java/com/kindnesskattle/bddAtcProject/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("/*")
                 .allowedOrigins("http://localhost:5501", "https://kindnesskettle.projects.bbdgrad.com","http://localhost:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
